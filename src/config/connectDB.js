ebffba14445d073c35f72e33ba0b7745e3c8cbaf
jascar_nns <jascar@neuralnestsolutions.com>
2025-11-26 12:29:00 +0530

    simplify mongodb connection like working example

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,27 +1,11 @@
 import mongoose from "mongoose";
 
-import dotenv from "dotenv";
-
 const connectDB = async () => {
-  if (!process.env.MONGO_URI) {
-    console.log("MONGO_URI environment variable is not set");
-    process.exit(1);
-  }
-
-  mongoose.set('bufferCommands', false);
-  
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      maxPoolSize: 10,
-      minPoolSize: 2,
-      maxIdleTimeMS: 30000,
-      serverSelectionTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
-      connectTimeoutMS: 30000
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.log("MongoDB connection failed:", error.message);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected");
+  } catch (err) {
+    console.log(err);
     process.exit(1);
   }
 };
